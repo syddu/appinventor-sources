f34f792f8c0c16f65f4c9f39946694dc0ab33e80
Sydney Du <sydneydu@gmail.com>
2025-10-30 12:37:35 -0400

    working LinearProgress progress updates

--- appinventor/components/src/com/google/appinventor/components/runtime/LinearProgress.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/LinearProgress.java
@@ -27,8 +27,6 @@ import com.google.appinventor.components.common.PropertyTypeConstants;
 import com.google.appinventor.components.common.YaVersion;
 
 import java.io.IOException;
-import jsinterop.annotations.JsMethod;
-import jsinterop.annotations.JsProperty;
 
 @DesignerComponent(
     version = YaVersion.LINEAR_PROGRESS_COMPONENT_VERSION,
@@ -39,7 +37,7 @@ import jsinterop.annotations.JsProperty;
 )
 @SimpleObject
 
-public final class LinearProgress extends AndroidViewComponent {
+public class LinearProgress extends AndroidViewComponent {
   private static final String LOG_TAG = "LinearProgress";
   private Context context;
   private ProgressBar progressBar;
@@ -53,7 +51,6 @@ public final class LinearProgress extends AndroidViewComponent {
     progressBar = new ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal);
 
     container.$add(this);
-
     Minimum(0);
     Maximum(100);
     ProgressColor(Component.COLOR_BLUE);
@@ -86,7 +83,6 @@ public final class LinearProgress extends AndroidViewComponent {
   @SimpleProperty(description = "Set the lower range of the progress bar to min. "
       + "This function works only for devices with API >= 26",
       category = PropertyCategory.BEHAVIOR)
-  @JsProperty(name = "Minimum")
   public void Minimum(int value) {
     if (VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       progressBar.setMin(value);
@@ -97,7 +93,6 @@ public final class LinearProgress extends AndroidViewComponent {
   }
 
   @SimpleProperty
-  @JsProperty(name = "Minimum")
   public int Minimum() {
     return VERSION.SDK_INT >= Build.VERSION_CODES.O ? progressBar.getMin() : 0;
   }
@@ -105,21 +100,18 @@ public final class LinearProgress extends AndroidViewComponent {
   @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_INTEGER, defaultValue = "100")
   @SimpleProperty(description = "Set the upper range of the progress bar max.",
       category = PropertyCategory.BEHAVIOR)
-  @JsProperty(name = "Maximum")
   public void Maximum(int value) {
     progressBar.setMax(value);
     Log.i(LOG_TAG, "setMax = " + value);
   }
 
   @SimpleProperty
-  @JsProperty(name = "Maximum")
   public int Maximum() {
     return progressBar.getMax();
   }
 
   @SimpleProperty(description = "Sets the current progress to the specified value. "
       + "Does not do anything if the progress bar is in indeterminate mode.")
-  @JsProperty(name = "Progress")
   public void Progress(int value) {
     if (VERSION.SDK_INT >= 24) {
       progressBar.setProgress(value, true);
@@ -130,7 +122,6 @@ public final class LinearProgress extends AndroidViewComponent {
   }
 
   @SimpleProperty(description = "Get the progress bar's current level of progress.")
-  @JsProperty(name = "Progress")
   public int Progress() {
     return progressBar.getProgress();
   }
@@ -145,7 +136,6 @@ public final class LinearProgress extends AndroidViewComponent {
       defaultValue = Component.DEFAULT_VALUE_COLOR_BLUE)
   @SimpleProperty(description = "Change the progress color of the progress bar.",
       category = PropertyCategory.APPEARANCE)
-  @JsProperty(name = "ProgressColor")
   public void ProgressColor(int color) {
     this.progressColor = color;
     Drawable drawable = progressBar.getProgressDrawable();
@@ -158,7 +148,6 @@ public final class LinearProgress extends AndroidViewComponent {
   }
 
   @SimpleProperty
-  @JsProperty(name = "ProgressColor")
   public int ProgressColor() {
     return this.progressColor;
   }
@@ -167,7 +156,6 @@ public final class LinearProgress extends AndroidViewComponent {
       defaultValue = Component.DEFAULT_VALUE_COLOR_BLUE)
   @SimpleProperty(description = "Change the indeterminate color of the progress bar.",
       category = PropertyCategory.APPEARANCE)
-  @JsProperty(name = "IndeterminateColor")
   public void IndeterminateColor(int color) {
     this.indeterminateColor = color;
     Drawable drawable = progressBar.getProgressDrawable();
@@ -180,7 +168,6 @@ public final class LinearProgress extends AndroidViewComponent {
   }
 
   @SimpleProperty
-  @JsProperty(name = "IndeterminateColor")
   public int IndeterminateColor() {
     return this.indeterminateColor;
   }
@@ -191,14 +178,12 @@ public final class LinearProgress extends AndroidViewComponent {
       + "In indeterminate mode, the progress is ignored and the "
       + "progress bar shows an infinite animation instead.",
       category = PropertyCategory.BEHAVIOR)
-  @JsProperty(name = "Indeterminate")
   public void Indeterminate(boolean enabled) {
     progressBar.setIndeterminate(enabled);
     Log.i(LOG_TAG, "Indeterminate is: " + enabled);
   }
 
   @SimpleProperty(description = "Indicate whether this progress bar is in indeterminate mode.")
-  @JsProperty(name = "Indeterminate")
   public boolean Indeterminate() {
     return progressBar.isIndeterminate();
   }
--- appinventor/webruntime/src/android/widget/ProgressBar.java
+++ appinventor/webruntime/src/android/widget/ProgressBar.java
@@ -5,9 +5,13 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.graphics.BlendModeColorFilter;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 
 import com.google.gwt.dom.client.Document;
+import com.google.appinventor.components.runtime.Component;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
@@ -20,9 +24,9 @@ public class ProgressBar extends View {
   private int mMin = 0;
   private int mMax = 100;
   private int mProgress = 0;
-
   private boolean mIndeterminate = false;
-
+  private int mProgressColor = Component.COLOR_BLUE;
+  private int mIndeterminateColor = Component.COLOR_BLUE;
   private Drawable mProgressDrawable;
 
   public ProgressBar(Context context) { this(DOM.createDiv()); }
@@ -40,18 +44,27 @@ public class ProgressBar extends View {
     getElement().getStyle().setDisplay(Style.Display.BLOCK);
     getElement().getStyle().setWidth(100, Style.Unit.PCT);
     getElement().getStyle().setProperty("minHeight", "8px");
-    // getElement().getStyle().setProperty("minWidth", "64px");
     getElement().getStyle().setOverflow(Style.Overflow.HIDDEN);
     mFill.getStyle().setDisplay(Style.Display.BLOCK);
     mFill.getStyle().setHeight(100, Style.Unit.PCT);
-
     getElement().getStyle().setBackgroundColor("rgba(0,0,0,.12)");
-    mFill.getStyle().setBackgroundColor("dodgerblue");
 
-    // Drawable not functional yet
-    mProgressDrawable = new Drawable() { };
-
-    refreshProgress();
+    mProgressDrawable = new Drawable() { 
+      public void setColorFilter(ColorFilter colorFilter) {
+        if (colorFilter instanceof BlendModeColorFilter) {
+          setColorFilter(((BlendModeColorFilter) colorFilter).getColor(), PorterDuff.Mode.SRC_IN);
+        }
+      }
+      public void setColorFilter(int color, PorterDuff.Mode mode) {
+        if (mIndeterminate) {
+          mIndeterminateColor = color;
+        } else {
+          mProgressColor = color;
+        }
+        updateColor(color);
+      }
+    };
+  refreshProgress();
   }
 
   public int getMin() { return mMin; }
@@ -101,6 +114,7 @@ public class ProgressBar extends View {
   public void setIndeterminate(boolean indeterminate) {
     if (mIndeterminate == indeterminate) return;
     mIndeterminate = indeterminate;
+    updateColor(mIndeterminate ? mIndeterminateColor : mProgressColor);
     refreshProgress();
   }
 
@@ -108,7 +122,6 @@ public class ProgressBar extends View {
     return mProgressDrawable;
   }
 
-  // Original function in Android source code is doRefreshProgress
   private void refreshProgress() {
     if (mIndeterminate) {
       mFill.getStyle().clearWidth();
@@ -120,4 +133,12 @@ public class ProgressBar extends View {
     if (ratio > 1) ratio = 1;
     mFill.getStyle().setWidth(ratio * 100.0, Style.Unit.PCT);
   }
+  private void updateColor(int color){
+        float alpha = ((color >> 24) & 0xFF) / 255.0f;
+        int r = (color >> 16) & 0xFF;
+        int g = (color >> 8) & 0xFF;
+        int b = color & 0xFF;
+        mFill.getStyle().setBackgroundColor("rgba(" + r + "," + g + "," + b + "," + alpha + ")");
+  }
 }
+  
